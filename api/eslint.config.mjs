92afb134d91417dbf8fd3a4f28cd9d299b26f844
WLEL <Lohenl@users.noreply.github.com>
2024-09-09 02:53:27 +0800

    updates eslint configs

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -5,5 +5,12 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     eslint.configs.recommended,
-    ...tseslint.configs.recommended,
+    // ...tseslint.configs.recommended,
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
+    {
+        rules: {
+            '@typescript-eslint/no-require-imports': 'off',
+        },
+    },
 );
