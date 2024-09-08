3f713e5c9d13e2497b18ba22c60c12c70421a653
WLEL <Lohenl@users.noreply.github.com>
2024-09-09 03:11:05 +0800

    updates eslint config

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -4,6 +4,10 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+    // global ignores
+    {
+        ignores: ["dist/", "node_modules/", "swagger-main.js", "swagger.js"],
+    },
     eslint.configs.recommended,
     // ...tseslint.configs.recommended,
     ...tseslint.configs.strict,
@@ -11,6 +15,11 @@ export default tseslint.config(
     {
         rules: {
             '@typescript-eslint/no-require-imports': 'off',
+        },
+    },
+    {
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off', // hehehe
         },
     },
 );
