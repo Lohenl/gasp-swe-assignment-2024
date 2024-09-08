8033eca16e458e8541cdd6772cd38ee9b272dda3
WLEL <Lohenl@users.noreply.github.com>
2024-09-09 02:30:45 +0800

    implements schemeseligibility api validation

--- api/src/functions/schemeseligiblity.ts
+++ api/src/functions/schemeseligiblity.ts
@@ -1,9 +1,10 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { Sequelize, DataTypes } from 'sequelize';
+import { Engine } from 'json-rules-engine';
+import Joi = require('joi');
 import SchemeModel from "../models/scheme";
 import BenefitModel from "../models/benefit";
 import ApplicantModel from "../models/applicant";
-import { Engine } from 'json-rules-engine';
 
 const sequelize = new Sequelize(process.env['PGDATABASE'], process.env['PGUSER'], process.env['PGPASSWORD'], {
     host: process.env['PGHOST'],
@@ -29,7 +30,6 @@ const sequelize = new Sequelize(process.env['PGDATABASE'], process.env['PGUSER']
 */
 export async function schemesEligibility(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     try {
-
         // declare 1:N Relationship
         // reference: https://sequelize.org/docs/v6/core-concepts/assocs/#one-to-many-relationships
         await sequelize.authenticate();
@@ -49,8 +49,8 @@ export async function schemesEligibility(request: HttpRequest, context: Invocati
         syncPromises.push(Applicant.sync());
         await Promise.allSettled(syncPromises);
 
-        // validation happens here, dont forget joi
-        context.log(request.query.get('id'));
+        context.debug('id:', request.query.get('id'));
+        Joi.assert(request.query.get('id'), Joi.string().guid().required());
         const applicant = await Applicant.findByPk(request.query.get('id'))
         if (!applicant) {
             return { status: 400, body: 'invalid ID provided' }
@@ -64,7 +64,6 @@ export async function schemesEligibility(request: HttpRequest, context: Invocati
 
         let evaluatedSchemes = [];
         let eligibleSchemes = [];
-
         let engine = new Engine();
 
         // load applicant details into rules engine as facts
@@ -86,21 +85,21 @@ export async function schemesEligibility(request: HttpRequest, context: Invocati
         let engineResults = await engine.run(facts);
         engineResults.results.forEach((result, index) => {
             // TODO: test assumption that rules are evaluated in the order of adding to engine
-            context.log('condition name:', result.name, ', result:', result.result);
+            context.debug('condition name:', result.name, ', result:', result.result);
             // if rule engine determines eligible, add to list of eligible schemes
             if (result.result) {
                 eligibleSchemes.push(evaluatedSchemes[index]);
             }
         });
 
-        // do json transformation here
+        // TODO: do json transformation here
         let jsonBody = eligibleSchemes;
         return { jsonBody }
 
     } catch (error) {
         context.error('schemes: error encountered:', error);
+        if (Joi.isError(error)) { return { status: 400, jsonBody: error } }
         return { status: 500, body: `Unexpected error occured: ${error}` }
-
     }
 };
 
