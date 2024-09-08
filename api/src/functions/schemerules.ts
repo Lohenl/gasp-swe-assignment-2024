3be86569773dfd643e56b709455acb5bf3a2b9d7
WLEL <Lohenl@users.noreply.github.com>
2024-09-09 02:17:45 +0800

    implements schemerules API validation

--- api/src/functions/schemerules.ts
+++ api/src/functions/schemerules.ts
@@ -1,7 +1,9 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { Sequelize, DataTypes } from 'sequelize';
+import Joi = require('joi');
 import SchemeModel from "../models/scheme";
 import BenefitModel from "../models/benefit";
+const validateJSON = require('../validators/schemeRulesValidate');
 
 const sequelize = new Sequelize(process.env['PGDATABASE'], process.env['PGUSER'], process.env['PGPASSWORD'], {
     host: process.env['PGHOST'],
@@ -195,7 +197,6 @@ const sequelize = new Sequelize(process.env['PGDATABASE'], process.env['PGUSER']
 */
 export async function schemeRules(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     try {
-
         // declare 1:N Relationship
         // reference: https://sequelize.org/docs/v6/core-concepts/assocs/#one-to-many-relationships
         await sequelize.authenticate();
@@ -213,16 +214,17 @@ export async function schemeRules(request: HttpRequest, context: InvocationConte
         await Promise.allSettled(syncPromises);
 
         if (request.method === 'GET') {
-            // validation happens here, dont forget joi
-            context.log(request.query.get('scheme_id'));
+            context.debug('id:', request.query.get('id'));
+            Joi.assert(request.query.get('id'), Joi.string().guid().required());
             const scheme = await Scheme.findByPk(request.query.get('scheme_id'));
             return { jsonBody: scheme.dataValues.eligibility_criteria }
 
         } else if (request.method === 'POST' || request.method === 'PATCH') {
-            // validation happens here, dont forget joi
-            context.log(request.query.get('scheme_id'));
+            context.debug('scheme_id:', request.query.get('scheme_id'));
             const schemeRuleToUpdate = await request.json() as any;
-            context.log('schemeRuleToUpdate:', schemeRuleToUpdate);
+            context.debug('schemeRuleToUpdate:', schemeRuleToUpdate);
+            Joi.assert(request.query.get('scheme_id'), Joi.string().guid().required());
+            validateJSON(schemeRuleToUpdate);
             // stringify the JSON structure for persistence
             const schemeRuleStringified = JSON.stringify(schemeRuleToUpdate);
             const scheme = await Scheme.findByPk(request.query.get('scheme_id'));
@@ -237,8 +239,8 @@ export async function schemeRules(request: HttpRequest, context: InvocationConte
             return { jsonBody: scheme.dataValues }
 
         } else if (request.method === 'DELETE') {
-            // validation happens here, dont forget joi
-            context.log(request.query.get('scheme_id'));
+            context.debug('scheme_id:', request.query.get('scheme_id'));
+            Joi.assert(request.query.get('scheme_id'), Joi.string().guid().required());
             const scheme = await Scheme.findByPk(request.query.get('scheme_id'));
             if (!scheme) {
                 return { status: 400, body: 'invalid scheme_id provided' }
@@ -246,13 +248,11 @@ export async function schemeRules(request: HttpRequest, context: InvocationConte
             scheme.update({ eligibility_criteria: null });
             await scheme.save();
             return { body: request.query.get('scheme_id') }
-
         }
-
     } catch (error) {
         context.error('schemes: error encountered:', error);
+        if (Joi.isError(error)) { return { status: 400, jsonBody: error } }
         return { status: 500, body: `Unexpected error occured: ${error}` }
-
     }
 };
 
